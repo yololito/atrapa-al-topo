b0ec46786117b2a9c6526289fd8c8149931a2bee
lolaa <123569523+yololito@users.noreply.github.com>
2023-05-08 12:01:32 -0500

    08-05

---
+++ app.js
@@ -0,0 +1,6 @@
+const cuadrado = document.querySelectorAll(".cuadrado");
+const tiempofaltante = document.querySelector("#tiempo");
+let puntaje = document.getElementById("puntaje");
+
+let resultado = 0;
+let tiempoactual = tiempofaltante.textContent;
